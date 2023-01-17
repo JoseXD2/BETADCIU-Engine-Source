d81a1d83a7aaaa228ae226b8bb0c77b5b73f17e2
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2023-01-17 18:10:43 -0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -196,6 +196,10 @@ class StoryMenuState extends MusicBeatState
 
 		trace("Line 165");
 
+		#if android
+                addVirtualPad(LEFT_FULL, A_B);
+                #end
+			
 		super.create();
 	}
 
